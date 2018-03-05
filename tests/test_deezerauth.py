33ed4eae400a5887dab564c92476d7e30a01bd6c
yaboroda <yarboroda@gmail.com>
2018-03-05 23:06:16 +0300

    start work on tests for deezerauth

---
+++ tests/test_deezerauth.py
@@ -0,0 +1,26 @@
+import pytest
+import pytest_mock
+from dztoolset.deezerauth import DeezerAuth
+
+assert callable(pytest_mock.mocker)
+
+class TestDeezerAuth(object):
+
+    def setup_class(self):
+        self.token = 'test_token'
+        self.port = 8090
+        self.secret = 'test_secret'
+        self.app_id = 123
+
+    def setup(self):
+        self.auth = DeezerAuth()
+        self.auth.set_params(self.port, self.secret, self.app_id, self.token)
+
+    def teardown(self):
+        self.auth = None
+
+    def test_created_instance(self):
+        assert self.auth._port == self.port
+        assert self.auth._secret == self.secret
+        assert self.auth._app_id == str(self.app_id)
+        assert self.auth.token == self.token
