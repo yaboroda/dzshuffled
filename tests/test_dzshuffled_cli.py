f24e22639b950290d20e53546cfc5db4b713b14a
yaboroda <yarboroda@gmail.com>
2018-03-06 18:38:13 +0300

    put test data in config for test_dzshuffled_cli

--- tests/test_dzshuffled_cli.py
+++ tests/test_dzshuffled_cli.py
@@ -1,9 +1,10 @@
 import os
-from unittest.mock import call
+from unittest.mock import call, patch
 import pytest
 import pytest_mock
 import dztoolset.dzshuffled_cli as dz_cli
 from dztoolset.printer import Printer
+from dztoolset.deezerconfig import DeezerConfig
 
 assert callable(pytest_mock.mocker)
 
@@ -12,12 +13,46 @@ class TestDzshuffledCli(object):
 
     def setup_class(self):
         self.config_path = './tests/testcfg.ini'
+        self.default_config_data = {
+            'system': {
+                'port': '8090',
+                'editor': 'vim'
+            },
+            'auth': {
+                'app_id': '123',
+                'secret': 'test_secret',
+                'token': 'test_token'
+            },
+            'pl_test1': {
+                'title': 'Test shuffled playlist',
+                'type': 'shuffled',
+                'source':
+                'playlist 1, playlist 2',
+                'limit': '100'
+            },
+            'pl_test2': {
+                'title': 'Test shuffled playlist 2',
+                'type': 'shuffled',
+                'source': 'playlist 3, playlist 4',
+                'limit': '200'
+            }
+        }
 
     def setup(self):
+        self.remove_test_config_file()
+        cfg_patcher = patch.object(
+            DeezerConfig,
+            '_get_default_data',
+            return_value=self.default_config_data
+        ).start()
         with pytest.raises(SystemExit):
             dz_cli.main([], self.config_path)
+        cfg_patcher.stop()
 
     def teardown(self):
+        self.remove_test_config_file()
+
+    def remove_test_config_file(self):
         if os.path.isfile(self.config_path):
             os.remove(self.config_path)
 
@@ -32,7 +67,8 @@ class TestDzshuffledCli(object):
             dz_cli.main(['-l'], self.config_path)
 
         Printer.print.assert_has_calls([
-            call('[0] pl_example'),
+            call('[0] pl_test1'),
+            call('[1] pl_test2'),
         ])
 
     def test_show_list_of_scenarios_verbous(self, mocker):
@@ -43,10 +79,26 @@ class TestDzshuffledCli(object):
         with pytest.raises(SystemExit):
             dz_cli.main(['-lv'], self.config_path)
 
-        Printer.print.assert_called_once_with('[0] pl_example')
+        Printer.print.assert_has_calls([
+            call('[0] pl_test1'),
+            call('[1] pl_test2'),
+        ])
 
-        Printer.pprint.assert_called_once_with({
-            'title': 'Example shuffled playlist',
-            'type': 'shuffled',
-            'source': 'playlist 1, playlist 2', 'limit': '1000'
-        })
+        Printer.pprint.assert_has_calls([
+            call(self.default_config_data['pl_test1']),
+            call(self.default_config_data['pl_test2']),
+        ])
+
+    def test_print_info_about_scenario_by_number(self, mocker):
+        mocker.patch.object(Printer, 'print')
+        mocker.patch.object(Printer, 'pprint')
+
+        # print short version, will use both methods (print, pprint)
+        with pytest.raises(SystemExit):
+            dz_cli.main(['-i', '0'], self.config_path)
+
+        Printer.print.assert_called_once_with('[0] pl_test1')
+
+        Printer.pprint.assert_called_once_with(
+            self.default_config_data['pl_test1']
+        )
