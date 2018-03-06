98b58e84ac58335c74d41e5ec43b19f7ea36da7f
yaboroda <yarboroda@gmail.com>
2018-03-06 17:17:16 +0300

    update test for output list of scenarios

--- tests/test_dzshuffled_cli.py
+++ tests/test_dzshuffled_cli.py
@@ -1,6 +1,11 @@
 import os
+from unittest.mock import call
 import pytest
+import pytest_mock
 import dztoolset.dzshuffled_cli as dz_cli
+from dztoolset.printer import Printer
+
+assert callable(pytest_mock.mocker)
 
 
 class TestDzshuffledCli(object):
@@ -16,9 +21,28 @@ class TestDzshuffledCli(object):
         if os.path.isfile(self.config_path):
             os.remove(self.config_path)
 
-    def test_show_list_of_scenarios(self, capsys):
+    def test_config_file_created(self):
         assert os.path.isfile(self.config_path)
+
+    def test_show_list_of_scenarios(self, mocker):
+        mocker.patch.object(Printer, 'print')
+        mocker.patch.object(Printer, 'pprint')
+
+        # print short version, will use print method
         with pytest.raises(SystemExit):
             dz_cli.main(['-l'], self.config_path)
-        out, err = capsys.readouterr()
-        assert 'pl_example' in out
+
+        # print verbouse version, will use both methods (print, pprint)
+        with pytest.raises(SystemExit):
+            dz_cli.main(['-lv'], self.config_path)
+
+        Printer.print.assert_has_calls([
+            call('[0] pl_example'),
+            call('[0] pl_example'),
+        ])
+
+        Printer.pprint.assert_called_once_with({
+            'title': 'Example shuffled playlist', 
+            'type': 'shuffled', 
+            'source': 'playlist 1, playlist 2', 'limit': '1000'
+        })
