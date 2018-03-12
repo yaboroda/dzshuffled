3453195751832bd652ecb0e4f5b79a141623c484
yaboroda <yarboroda@gmail.com>
2018-03-12 16:46:43 +0300

    update tests for deezertool

--- tests/test_deezertool.py
+++ tests/test_deezertool.py
@@ -1,9 +1,12 @@
 import os
 import pytest
+import pytest_mock
 from dztoolset.deezertool import DeezerTool
 from dztoolset.deezerauth import DeezerAuth
 from dztoolset.deezerapi import DeezerApi
 from dztoolset.deezerconfig import DeezerConfig
+
+assert callable(pytest_mock.mocker)
 
 
 class TestDeezerTool(object):
@@ -27,3 +30,12 @@ class TestDeezerTool(object):
         assert isinstance(self.tool.config, DeezerConfig)
         assert isinstance(self.tool.auth, DeezerAuth)
         assert isinstance(self.tool.api, DeezerApi)
+
+    def test_user_prop(self, mocker):
+        user_data = {"userdata": "data"}
+        mocker.patch.object(DeezerAuth, 'user', return_value=user_data)
+
+        user = self.tool.user()
+
+        assert user == user_data
+        DeezerAuth.user.assert_called_once()
